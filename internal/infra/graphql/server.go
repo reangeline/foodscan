aebc68394fc2cbe402660978a5a85e7eb19b7ce8
Renato Angeline <renatoangeline@Renato-MacBook-Pro.local>
2023-06-20 22:10:10 -0300

    added graphql

---
+++ internal/infra/graphql/server.go
@@ -0,0 +1,28 @@
+package graphql
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/reangeline/foodscan_backend/internal/infra/graphql/graph"
+)
+
+const defaultPort = "8080"
+
+func ServerGQL() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
